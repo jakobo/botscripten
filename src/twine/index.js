414b17e28408b7de1a21662a79e1c4548a2895fc
Jakob Heuser <jakob@felocity.com>
2020-01-02 19:53:02 -0800

    fix: updated output to include proofing mode

--- src/twine/index.js
+++ src/twine/index.js
@@ -5,10 +5,15 @@ import Story from "./Story";
     win.document.addEventListener("DOMContentLoaded", function(event) {
       win.globalEval = eval;
       win.story = new Story(win);
+      win.story.start();
       if (win.document.querySelector("#show_directives").checked) {
         win.document.body.classList.add("show-directives");
       }
-      win.story.start();
+      if (win.document.querySelector("#proofing").checked) {
+        win.document.body.classList.add("proof");
+      } else {
+        win.document.body.classList.add("run");
+      }
     });
 
     win.document
@@ -20,5 +25,23 @@ import Story from "./Story";
           win.document.body.classList.remove("show-directives");
         }
       });
+
+    win.document.querySelector("#proofing").addEventListener("change", e => {
+      if (e.target.checked) {
+        win.document.body.classList.add("proof");
+        win.document.body.classList.remove("run");
+      } else {
+        win.document.body.classList.add("run");
+        win.document.body.classList.remove("proof");
+      }
+    });
+
+    document
+      .querySelector(
+        "tw-passagedata[pid='" +
+          document.querySelector("tw-storydata").getAttribute("startnode") +
+          "']"
+      )
+      .classList.add("start");
   }
 })(window || undefined);
