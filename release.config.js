1bb652f8101827471b06d5d51737898eda6ee02d
Jakob Heuser <jakob@felocity.com>
2020-03-03 17:10:18 -0800

    ♻️ Refactor: Updates more of semantic-release's information
    Moves us to cz-emoji (more support) and updates the release.config.js to contain git syntax

--- release.config.js
+++ release.config.js
@@ -1,8 +1,21 @@
-{
+module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
     "@semantic-release/npm",
-    "@semantic-release/github",
-  ];
-}
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/**/*", "CHANGELOG.md"],
+        message:
+          "🧹 Chore: Releases ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
