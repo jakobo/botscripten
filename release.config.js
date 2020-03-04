1d278df0381d6e4eac3ac4f49d122c1a7c9b966c
Jakob Heuser <jakob@felocity.com>
2020-03-03 20:21:15 -0800

    🐛 Fix: Fixes semantic-release to work with our release style
    We needed to build our conventional changelog from the emoji syntax, which was a bit of a chore

--- release.config.js
+++ release.config.js
@@ -2,11 +2,55 @@ const fs = require("fs");
 const czrc = JSON.parse(fs.readFileSync("./.czrc", "utf8"));
 
 const generateTypes = () =>
-  czrc.config["cz-emoji"].types.map(t => ({
-    type: t.name,
-    section: t.changelog ? t.changelog.section || "" : "",
-    hidden: !t.changelog || !t.changelog.section ? true : false,
-  }));
+  czrc.config["cz-emoji"].types.map(t => {
+    const opts = {
+      type: t.name.toLowerCase(),
+      hidden: !t.changelog || !t.changelog.section ? true : false,
+    };
+    if (t.changelog && t.changelog.section) {
+      opts.section = t.changelog.section;
+    }
+    return opts;
+  });
+
+const generateReleaseRules = () =>
+  czrc.config["cz-emoji"].types
+    .map(t => {
+      if (!t.release) {
+        return false;
+      }
+      if (typeof t.release === "string") {
+        return {
+          type: t.name,
+          release: t.release,
+        };
+      }
+
+      // lots of options
+      let opts = {};
+      opts.type = t.name;
+      if (t.release.scope) {
+        opts.scope = t.release.scope;
+        opts.release = t.release.release;
+      }
+      return opts;
+    })
+    .filter(Boolean);
+
+const ccOptions = {
+  releaseRules: generateReleaseRules(),
+  parserOpts: {
+    headerPattern: /^.+?\s+(\w+)(?:\((.+)\))?!?: (.+)$/, // emoji friendly
+    headerCorrespondence: ["type", "scope", "subject"],
+  },
+  linkReferences: true,
+  presetConfig: {
+    header: "Changelog",
+    types: generateTypes(),
+    releaseCommitMessageFormat:
+      "🧹 Chore: Adds changelog for {{currentTag}} [skip ci]",
+  },
+};
 
 module.exports = {
   plugins: [
@@ -14,18 +58,16 @@ module.exports = {
       "@semantic-release/commit-analyzer",
       {
         preset: "conventionalcommits",
-        parserOpts: {
-          headerPattern: /^.+?\s(\w*)(?:\((.*)\))?!?: (.*)$/, // emoji friendly
-        },
-        presetConfig: {
-          header: "Changelog",
-          types: generateTypes(),
-          releaseCommitMessageFormat:
-            "🧹 Chore: Adds changelog for {{currentTag}} [skip ci]",
-        },
+        ...ccOptions,
       },
     ],
-    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        ...ccOptions,
+      },
+    ],
     [
       "@semantic-release/changelog",
       {
