8783bf26fee0925a8aa362d65cd25a70db80623a
Jakob Heuser <jakob@felocity.com>
2020-03-03 17:37:33 -0800

    ♻️ Refactor: Uses czrc file to generate conventional-changelog values

--- release.config.js
+++ release.config.js
@@ -1,6 +1,30 @@
+const fs = require("fs");
+const czrc = JSON.parse(fs.readFileSync("./.czrc", "utf8"));
+
+const generateTypes = () =>
+  czrc.config["cz-emoji"].types.map(t => ({
+    type: t.name,
+    section: t.changelog ? t.changelog.section || "" : "",
+    hidden: !t.changelog || !t.changelog.section ? true : false,
+  }));
+
 module.exports = {
   plugins: [
-    "@semantic-release/commit-analyzer",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventional-changelog",
+        parserOpts: {
+          headerPattern: /^.+?\s(\w*)(?:\((.*)\))?!?: (.*)$/, // emoji friendly
+        },
+        presetConfig: {
+          header: "Changelog",
+          types: generateTypes(),
+          releaseCommitMessageFormat:
+            "🧹 Chore: Adds changelog for {{currentTag}} [skip ci]",
+        },
+      },
+    ],
     "@semantic-release/release-notes-generator",
     [
       "@semantic-release/changelog",
